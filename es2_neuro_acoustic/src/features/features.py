f70d26cbce3a76f778699fcfeac4e1dfdf1cf670
Gerardo Antonio Cecere <gerardoa.cecere@gmail.com>
2026-05-25 18:13:59 +0200

    Delete es2_neuro_acoustic directory

--- es2_neuro_acoustic/src/features/features.py
+++
@@ -1,6 +0,0 @@
-from src.features.bands_power import BandsPower
-
-
-class FeatureExtractor(BandsPower):
-    def __init__(self):
-        BandsPower.__init__(self)
--- es2_neuro_acoustic/src/recording/recording.py
+++
@@ -1,6 +0,0 @@
-from src.recording.recording_data import RecordingData
-
-
-class Recording(RecordingData):
-    def __init__(self):
-        RecordingData.__init__(self)
